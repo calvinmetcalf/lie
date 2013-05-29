7d927804900e8d18824d7898833228c4a9446a25
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-29 09:54:48 -0400

    no need for two builds, also its async now

--- build/build.js
+++ build/build.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 var fs = require("fs");
 var UglifyJS = require("uglify-js");
-
-var copyright = fs.readFileSync("promiscuous.js", "utf8").match(/.*\n/)[0];
-var minified = copyright + UglifyJS.minify("promiscuous.js").code;
-
 var path = "dist/";
-if(!fs.existsSync(path))
-  fs.mkdirSync(path);
 
-fs.writeFileSync(path + "promiscuous-node.js",
-                 minified);
-fs.writeFileSync(path + "promiscuous-browser.js",
-                 minified.replace("module.exports", "window.promiscuous")
-                         .replace("process.nextTick", "setTimeout"));
+fs.readFile("promiscuous.js", "utf8",function(err,cr){
+  var copyright = cr.match(/.*\n/)[0];
+  var minified = copyright + UglifyJS.minify("promiscuous.js").code;
+  fs.exists(path,function(exists){
+    if(!exists){
+      fs.mkdir(path,function(){
+        fs.writeFile(path + "promiscuous.js",minified,function(){console.log("done")});
+      });
+    }else{
+      fs.writeFile(path + "promiscuous.js",minified,function(){console.log("done")});
+    }
+  });
+});
+
