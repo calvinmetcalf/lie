b2cc40ee0fab6327e86f5d8dd255ff67d5881edd
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-29 11:52:29 -0400

    now that's an easier build file

--- build/build.js
+++ build/build.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 var fs = require("fs");
-var UglifyJS = require("uglify-js");
-var path = "dist/";
-
-fs.readFile("promiscuous.js", "utf8",function(err,cr){
-  var copyright = cr.match(/.*\n/)[0];
-  var minified = copyright + UglifyJS.minify("promiscuous.js").code;
-  fs.exists(path,function(exists){
-    if(!exists){
-      fs.mkdir(path,function(){
-        fs.writeFile(path + "promiscuous.js",minified,function(){console.log("done")});
-      });
-    }else{
-      fs.writeFile(path + "promiscuous.js",minified,function(){console.log("done")});
-    }
-  });
+var spawn = require('child_process').spawn;
+var path = './dist';
+fs.exists(path,function(exists){
+   if(!exists){
+     fs.mkdir(path,function(){
+       makeIt();
+     });
+   }else{
+     makeIt();
+   }
 });
-
+function makeIt(){
+spawn("./node_modules/uglify-js/bin/uglifyjs", ['promiscuous.js', '--comments', '-o', 'dist/promiscuous.js']);
+}
