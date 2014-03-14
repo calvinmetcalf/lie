92b217b614254c27c6639910f3f95cb787414060
Calvin Metcalf <cmetcalf@appgeo.com>
2014-03-14 13:29:35 -0400

    all with tests!

--- test/cover.js
+++ test/cover.js
@@ -3,6 +3,7 @@ var aplus = require('promises-aplus-tests');
 var Promise = require('../lib');
 var adapter = {};
 var INTERNAL = require('../lib/INTERNAL');
+var assert = require('assert');
 
 adapter.deferred = function () {
   var pending = {};
@@ -131,6 +132,94 @@ describe('Lie', function () {
           done(true);
         }
       });
+    });
+  });
+  describe('Promise.all', function () {
+    //https://github.com/domenic/promises-unwrapping/blob/master/reference-implementation/test/all.js
+    it('fulfills if passed an empty array', function (done) {
+      var iterable = [];
+
+      Promise.all(iterable).then(function (value) {
+        assert(Array.isArray(value));
+        assert.deepEqual(value, []);
+        done();
+      });
+    });
+
+    it('fulfills if passed an array of mixed fulfilled promises and values', function (done) {
+      var iterable = [0, Promise.resolve(1), 2, Promise.resolve(3)];
+
+      Promise.all(iterable).then(function (value) {
+        assert(Array.isArray(value));
+        assert.deepEqual(value, [0, 1, 2, 3]);
+        done();
+      });
+    });
+
+    it('rejects if any passed promise is rejected', function (done) {
+      var foreverPending = new Promise(function () {});
+      var error = new Error('Rejected');
+      var rejected = Promise.reject(error);
+
+      var iterable = [foreverPending, rejected];
+
+      Promise.all(iterable).then(
+        function (value) {
+          assert(false, 'should never get here');
+          done();
+        },
+        function (reason) {
+          assert.strictEqual(reason, error);
+          done();
+        }
+      );
+    });
+
+    it('resolves foreign thenables', function (done) {
+      var normal = Promise.resolve(1);
+      var foreign = { then: function (f) { f(2); } };
+
+      var iterable = [normal, foreign];
+
+      Promise.all(iterable).then(function (value) {
+        assert.deepEqual(value, [1, 2]);
+        done();
+      });
+    });
+
+    it('fulfills when passed an sparse array, giving `undefined` for the omitted values', function (done) {
+      var iterable = [Promise.resolve(0), , , Promise.resolve(1)];
+
+      Promise.all(iterable).then(function (value) {
+        assert.deepEqual(value, [0, undefined, undefined, 1]);
+        done();
+      });
+    });
+
+    it('does not modify the input array', function (done) {
+      var input = [0, 1];
+      var iterable = input;
+
+      Promise.all(iterable).then(function (value) {
+        assert.notStrictEqual(input, value);
+        done();
+      });
+    });
+
+
+    it('should reject with a TypeError if given a non-iterable', function (done) {
+      var notIterable = {};
+
+      Promise.all(notIterable).then(
+        function () {
+          assert(false, 'should never get here');
+          done();
+        },
+        function (reason) {
+          assert(reason instanceof TypeError);
+          done();
+        }
+      );
     });
   });
   describe('Promises/A+ Tests', function () {
