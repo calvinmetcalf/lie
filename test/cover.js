a1654d4e2c4aa55e2d91d3f9f364b4d381e51e54
Calvin Metcalf <cmetcalf@appgeo.com>
2014-03-14 10:00:56 -0400

    simpler, 100% coverage

--- test/cover.js
+++ test/cover.js
@@ -1,21 +1,62 @@
 'use strict';
-
-var promise = require('../lib/lie');
+var aplus = require('promises-aplus-tests');
+var Promise = require('../lib');
 var adapter = {};
-//based off rsvp's adapter
+var INTERNAL = require('../lib/INTERNAL');
+
 adapter.deferred = function () {
   var pending = {};
-  pending.promise = new promise(function (resolve, reject) {
-    pending.resolve = resolve;
+  pending.promise = new Promise(function (resolver, reject) {
+    pending.resolve = resolver;
     pending.reject = reject;
   });
-  
-
   return pending;
 };
-adapter.rejected = function (reason) {
-  return promise(function () {
-    throw reason;
+adapter.resolved = Promise.resolve;
+adapter.rejected = Promise.reject;
+//noop, just for coverage
+INTERNAL();
+
+describe('Lie', function () {
+  it('should work without new', function (done) {
+    Promise(function (resolve) {
+      resolve(true);
+    }).then(function () {
+      done();
+    });
   });
-};
-module.exports = adapter;
+  it('should work resolving a promise new', function (done) {
+    new Promise(function (resolve) {
+      resolve(new Promise(function (resolve) {
+        resolve(true);
+      }));
+    }).then(function (result) {
+      if (result === true) {
+        done();
+      } else {
+        done(true);
+      }
+    });
+  });
+  it('should throw if you don\'t pass a function', function (done) {
+    try {
+      new Promise(true);
+    } catch (e) {
+      if (e instanceof TypeError) {
+        done();
+      } else {
+        done(e);
+      }
+    }
+  });
+  it('should have a working catch method', function (done) {
+    new Promise(function () {
+      throw new Error('boom');
+    }).catch(function () {
+      done();
+    });
+  });
+  describe('Promises/A+ Tests', function () {
+      aplus.mocha(adapter);
+  });
+});
