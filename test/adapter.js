f5105e6303db18bc2f324fb5b58c1446403a7409
Calvin Metcalf <calvin.metcalf@state.ma.us>
2014-03-13 20:54:41 -0400

    more progress

--- test/adapter.js
+++ test/adapter.js
@@ -1,24 +1,20 @@
 'use strict';
-var Promise = require('../lib/lie');
+var Promise = require('../lib/index');
 var promisesAplusTests = require('promises-aplus-tests');
 var adapter = {};
 
 adapter.deferred = function () {
   var pending = {};
-  pending.promise = new Promise();
-  pending.resolve = function (value) {
-    return pending.promise.resolve(value);
-  };
-  pending.reject = function (value) {
-    return pending.promise.reject(value);
-  };
+  pending.promise = new Promise(function (resolver, reject) {
+  	pending.resolve = resolver;
+  	pending.reject = reject;
+  });
   return pending;
 };
-
+adapter.resolved = Promise.resolve;
+adapter.reject = Promise.reject;
 promisesAplusTests(adapter, {
-  reporter: 'spec'//,
-  // timeout: '500000',
-  // grep:'2.3.3: Otherwise, if `x` is an object or function, 2.3.3.3: If `then` is a function, call it with `x` as `this`, first argument `resolvePromise`, and second argument `rejectPromise` 2.3.3.3.1: If/when `resolvePromise` is called with value `y`, run `[[Resolve]](promise, y)` `y` is a thenable `y` is a thenable that fulfills but then throws `then` calls `resolvePromise` asynchronously via return from a fulfilled promise'
+  reporter: 'spec'
 }, function () {
   console.log('done');
   process.exit();
